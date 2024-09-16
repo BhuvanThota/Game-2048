6a3e38cefe578546e45b337adc3bfc63305b851f
BhuvanThota <teacherb3337@gmail.com>
2024-09-16 06:31:42 +0530

    Game Logic Done

--- Tile.js
+++ Tile.js
@@ -1,14 +1,20 @@
+
 export default class Tile{
     #tileElement
     #x
     #y
     #value
 
-    constructor(tileContainer, value = Math.random() > .5 ? 2:4){
+    constructor(tileContainer, value = Math.random() > .5 ? 2: 4 ){
+
         this.#tileElement = document.createElement("div")
         this.#tileElement.classList.add('tile')
         tileContainer.append(this.#tileElement)
-        this.value = value 
+        this.value = value
+    }
+
+    get value(){
+        return this.#value
     }
 
     set x(value){
@@ -24,12 +30,36 @@ export default class Tile{
         this.#value = v
         this.#tileElement.textContent = v
         const power = Math.log2(v)
-        const bgHue = 18 + power * 3
-        const bgSat = 40 + power * 5
-        const bgLight = Math.floor(80 - power * 3)
+        let numColor = "#000";
+        if (power >= 9) {
+            numColor = "#FFF";
+        }else if( power > 5 && power < 9 ){
+            numColor = "#fe072c";
+        }else if( power > 3 && power < 6 ){
+            numColor = "#98021b";
+        }else{
+            numColor = "#000";
+        }
+        const bgHue = Math.floor(15 + power * 3) 
+        const bgSat = 50 + power * 6
+        const bgLight = Math.floor(100 - power * 4.75)
+         
         this.#tileElement.style.setProperty("--bg-hue", `${bgHue}`)
         this.#tileElement.style.setProperty("--bg-sat", `${bgSat}%`)
         this.#tileElement.style.setProperty("--bg-light", `${bgLight}%`)
+        this.#tileElement.style.setProperty("--num-color", numColor )
+    }
 
+    remove(){
+        this.#tileElement.remove()
+    }
+
+    waitForTransition(animation = false){
+        return new Promise(resolve =>{
+            this.#tileElement.addEventListener(animation ? "animationend" :"transitionend", resolve, {
+                once: true,
+            })
+        }
+        )
     }
 }
