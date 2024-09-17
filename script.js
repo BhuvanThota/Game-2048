fa36ca2efe3b02ed3ff3f7e07973615e05b09495
BhuvanThota <teacherb3337@gmail.com>
2024-09-17 21:33:58 +0530

    Update Reset Button

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
 import Grid from "./Grid.js";
 import Tile from "./Tile.js";
 
-const HIGH_SCORE_KEY = 'highScore'
-
-
-let score = 0;
-let highScore = localStorage.getItem(HIGH_SCORE_KEY) || 0
-
+const HIGH_SCORE_KEY = "highScore";
+const gameBoard = document.getElementById("game-board");
+const resetButton = document.getElementById("reset-button");
 const highScoreElement = document.getElementById("high-score");
 export let scoreElement = document.getElementById("score");
 
-highScoreElement.innerHTML = `High Score: <br> ${highScore} `
+resetButton.removeEventListener("click", resetGame);
+let score = 0;
+let highScore = localStorage.getItem(HIGH_SCORE_KEY) || 0;
+
+highScoreElement.innerHTML = `High Score: <br> ${highScore} `;
 
 export function updateScore(newPoints) {
-    score += newPoints;
-    scoreElement.innerHTML = `Score: <br> ${score} `
+  score += newPoints;
+  scoreElement.innerHTML = `Score: <br> ${score} `;
 
-    if (score > highScore) {
-        highScore = score;
-        localStorage.setItem(HIGH_SCORE_KEY, highScore);  
-        highScoreElement.innerHTML = `High Score: <br> ${highScore} `
-    }
+  if (score > highScore) {
+    highScore = score;
+    localStorage.setItem(HIGH_SCORE_KEY, highScore);
+    highScoreElement.innerHTML = `High Score: <br> ${highScore} `;
+  }
 }
 
-const resetButton = document.getElementById("reset-button");
-
-resetButton.onclick = resetGame;
-
-const gameBoard = document.getElementById("game-board");
-
-const grid = new Grid(gameBoard);
+let grid = new Grid(gameBoard);
 let startX, startY, endX, endY;
 
 grid.randomEmptyCell().tile = new Tile(gameBoard);
@@ -37,77 +32,80 @@ grid.randomEmptyCell().tile = new Tile(gameBoard);
 
 setupInput();
 
-
 function setupInput() {
   window.addEventListener("keydown", handleInput, { once: true });
-  gameBoard.addEventListener("touchstart", handleTouchStart, {once:true, passive: false});
+  gameBoard.addEventListener("touchstart", handleTouchStart, {
+    once: true,
+    passive: false,
+  });
+  resetButton.addEventListener("click", resetGame);
 }
 
-async function handleTouchStart(e){
+async function handleTouchStart(e) {
   stopInput();
   e.preventDefault();
   let keyValue;
-  const touch =  e.touches[0];
+  const touch = e.touches[0];
   startX = touch.clientX;
   startY = touch.clientY;
 
-  gameBoard.addEventListener("touchend", handleTouchEnd , {once:true});
+  gameBoard.addEventListener("touchend", handleTouchEnd, { once: true });
 }
-  
-  async function handleTouchEnd(event) {
-    const touch = event.changedTouches[0];
-    endX = touch.clientX;
-    endY = touch.clientY;
-  
-    const dx = endX - startX;
-    const dy = endY - startY;
-  
-    if (Math.abs(dx) > Math.abs(dy)) {
-      if (dx > 0) {
-        if (!canMoveRight()) {
-          setupInput();
-          return;
-        }
-        await moveRight();
-      } else {
-        if (!canMoveLeft()) {
-          setupInput();
-          return;
-        }
-        await moveLeft();
+
+async function handleTouchEnd(event) {
+  const touch = event.changedTouches[0];
+  endX = touch.clientX;
+  endY = touch.clientY;
+
+  const dx = endX - startX;
+  const dy = endY - startY;
+
+  if (Math.abs(dx) > Math.abs(dy)) {
+    if (dx > 0) {
+      if (!canMoveRight()) {
+        setupInput();
+        return;
       }
+      await moveRight();
     } else {
-      if (dy > 0) {
-        if (!canMoveDown()) {
-          setupInput();
-          return;
-        }
-        await moveDown();
-      } else {
-        if (!canMoveUp()) {
-          setupInput();
-          return;
-        }
-        await moveUp();
+      if (!canMoveLeft()) {
+        setupInput();
+        return;
       }
+      await moveLeft();
     }
-  
+  } else {
+    if (dy > 0) {
+      if (!canMoveDown()) {
+        setupInput();
+        return;
+      }
+      await moveDown();
+    } else {
+      if (!canMoveUp()) {
+        setupInput();
+        return;
+      }
+      await moveUp();
+    }
+  }
+
   grid.cells.forEach((cell) => cell.mergeTiles());
   const newTile = new Tile(gameBoard);
   grid.randomEmptyCell().tile = newTile;
 
-  if(!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight() ){
+  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
     newTile.waitForTransition(true).then(() => {
-        alert("Game Over")
-    })
-    return 
+      alert("Game Over");
+    });
+    return;
   }
 
   setupInput();
 }
 
 async function handleInput(e) {
-  stopInput()
+  stopInput();
   switch (e.key) {
     case "ArrowUp":
     case "w":
@@ -150,21 +148,20 @@ async function handleInput(e) {
   const newTile = new Tile(gameBoard);
   grid.randomEmptyCell().tile = newTile;
 
-  if(!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight() ){
+  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
     newTile.waitForTransition(true).then(() => {
-        alert("Game Over")
-    })
-    return 
+      alert("Game Over");
+    });
+    return;
   }
 
   setupInput();
 }
 
-function stopInput(){
+function stopInput() {
   window.removeEventListener("keydown", handleInput);
   gameBoard.removeEventListener("touchstart", handleTouchStart);
 }
-
 
 function moveUp() {
   return slideTiles(grid.cellsByColumn);
@@ -210,33 +207,32 @@ function slideTiles(cells) {
   );
 }
 
-function canMove(cells){
-    return cells.some(group => {
-        return group.some((cell, index) => {
-            if (index === 0) return false
-            if (cell.tile == null) return false
-            const moveToCell = group[index -1]
-            return moveToCell.canAccept(cell.tile)
-        })
-    })
+function canMove(cells) {
+  return cells.some((group) => {
+    return group.some((cell, index) => {
+      if (index === 0) return false;
+      if (cell.tile == null) return false;
+      const moveToCell = group[index - 1];
+      return moveToCell.canAccept(cell.tile);
+    });
+  });
 }
 
-function canMoveUp(){
-    return canMove(grid.cellsByColumn)
+function canMoveUp() {
+  return canMove(grid.cellsByColumn);
 }
 
-function canMoveDown(){
-    return canMove(grid.cellsByColumn.map((col) => [...col].reverse()))
+function canMoveDown() {
+  return canMove(grid.cellsByColumn.map((col) => [...col].reverse()));
 }
 
-function canMoveLeft(){
-    return canMove(grid.cellsByRow)
+function canMoveLeft() {
+  return canMove(grid.cellsByRow);
 }
 
-function canMoveRight(){
-    return canMove(grid.cellsByRow.map((row) => [...row].reverse()))
+function canMoveRight() {
+  return canMove(grid.cellsByRow.map((row) => [...row].reverse()));
 }
-
 
 function resetGame() {
   stopInput();
@@ -245,14 +241,13 @@ function resetGame() {
   scoreElement.innerText = `Score: ${score}`;
 
   // Clear the game board
-  gameBoard.innerHTML = ''; // Remove all tiles
-  
+  gameBoard.innerHTML = ""; // Remove all tiles
+
   // Reinitialize the grid and add two new tiles
-  const grid = new Grid(gameBoard);
+  grid = new Grid(gameBoard);
   grid.randomEmptyCell().tile = new Tile(gameBoard);
   grid.randomEmptyCell().tile = new Tile(gameBoard);
 
   // Set up input again
   setupInput();
 }
-
